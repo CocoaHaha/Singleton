a94e1440754a406ebd11ee79c8225732355769cb
SXF <sxf@sxfdeimac.local>
2016-12-02 10:11:17 +0800

    Initial Commit

---
+++ 单例模式/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  单例模式
+//
+//  Created by SXF on 16/12/2.
+//  Copyright © 2016年 SXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 单例模式/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  单例模式
+//
+//  Created by SXF on 16/12/2.
+//  Copyright © 2016年 SXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
